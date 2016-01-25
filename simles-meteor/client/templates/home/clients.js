b969c456c0c744f2a2a4362de1b3d29478401c2f
Jose Mauricio Delgado <jdelgado@greetail.co>
2016-01-25 00:25:58 -0600

    limiting loads to 20 items, adding -more- button to load more clients, formatted clients, and made it look better on mobile

--- simles-meteor/client/templates/home/clients.js
+++ simles-meteor/client/templates/home/clients.js
@@ -1,9 +1,17 @@
-Meteor.subscribe('clients');
+var ITEMS_INCREMENT = 20;
+Session.setDefault('clientsLimit', ITEMS_INCREMENT);
+
+Deps.autorun(function() {
+  Meteor.subscribe('clients', Session.get('clientsLimit'));
+});
 
 Template.clients.helpers({
 	clients: function(){
 		return Clients.find({});
-	}	
+	},
+	moreResults: function() {
+		return !(Clients.find().count() < Session.get("clientsLimit"));
+	}
 });
 
 Template.clients.events({
@@ -15,5 +23,11 @@ Template.clients.events({
     Meteor.call("addClient", name);
 
     template.$('[name=clientName]').val("");
+    },
+    "click .more-clients": function(event, template){
+    	event.preventDefault();
+    	Session.set('clientsLimit', Session.get('clientsLimit') + ITEMS_INCREMENT);
     }
-});
+}
+	
+);
--- simles-meteor/server/publish.js
+++ simles-meteor/server/publish.js
@@ -1,14 +1,16 @@
-Meteor.publish('clients', function() {
+Meteor.publish('clients', function(limit) {
   if (this.userId) {
-    return Clients.find({owner: this.userId});
+    return Clients.find({owner: this.userId}, 
+      {limit: limit});
   } else {
     this.ready();
   }
 });
 
-Meteor.publish('contacts', function() {
+Meteor.publish('contacts', function(limit) {
   if (this.userId) {
-    return Contacts.find({owner: this.userId});
+    return Contacts.find({owner: this.userId}, 
+      {limit: limit});
   } else {
     this.ready();
   }
