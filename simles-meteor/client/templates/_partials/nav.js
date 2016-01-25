7c277d56e17d572b59880f1e12c92f7af49ea1e3
Jose Mauricio Delgado <jdelgado@greetail.co>
2016-01-25 00:57:09 -0600

    setting active class on selected element in navbar

--- simles-meteor/client/templates/_partials/nav.js
+++ simles-meteor/client/templates/_partials/nav.js
@@ -2,5 +2,10 @@ Template.nav.events({
 	'click .logout' : function(){
 		Meteor.logout();
 		Router.go('/');
+	},
+	"click .nav a" :function(event, template){
+		template.$(".nav").find(".active").removeClass("active");
+		template.$(this).parent().addClass("active");
 	}
 });
+
